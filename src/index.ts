0143cc225413e323593d4675c3d6cd6eb550149a
Muhammad Ali Rehman <alirehman@pk.folio3.com>
2023-05-03 17:48:58 -0700

    Express and Apollo Server setup

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,33 @@
 import "reflect-metadata";
 import { createConnection } from "typeorm";
+import { createServer } from "http";
 import { ApolloServer, AuthenticationError } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
+import express from "express";
+import { HelloResolver } from "./resolvers/Temp";
+import { ApolloServerPluginLandingPageGraphQLPlayground ,ApolloServerPluginDrainHttpServer} from "apollo-server-core";
 
-async function main() {
-  const connection = await createConnection()
-  const schema = await buildSchema()
-  const server = new ApolloServer({ schema })
-  await server.listen(4000)
-  console.log("Server has started!")
-}
-main()
+const main=async ()=> {
+  await createConnection()
+    .then((connection) => {})
+    .catch((error) => console.log(error));
+    const apolloServer=new ApolloServer({
+      schema:await buildSchema({
+        resolvers:[HelloResolver],
+        validate:false
+        
+      }),
+      plugins: [
+        ApolloServerPluginLandingPageGraphQLPlayground()
+      ]
+    })
+    await apolloServer.start()
+    const app=express();
+    apolloServer.applyMiddleware({ app });
+    app.get("/", (_req, res) => res.send("Working"));
+    const serverport=process.env.SERVER_PORT
+    app.listen(serverport, () => console.log(`server started on port ${serverport}`));
+
+    
+  }
+main().catch((err)=>console.error(err));
---
+++ src/models/Category.ts
@@ -0,0 +1,42 @@
+import{
+    Column,
+    CreateDateColumn,
+    Entity,
+    PrimaryGeneratedColumn,
+    Unique,
+    UpdateDateColumn,
+    OneToMany
+}
+from "typeorm";
+import { Product } from "./Product";
+import { Field, ObjectType } from "type-graphql";
+
+
+@ObjectType()
+@Entity()
+@Unique(["code"])
+export class Category {
+  @Field((_type) => Number)
+  @PrimaryGeneratedColumn()
+  public readonly id!: number;
+
+  @Field()
+  @Column({ type: "varchar" })
+  public code!: string;
+
+  @Field()
+  @Column({ type: "varchar" })
+  public name!: string;
+
+  @Field()
+  @CreateDateColumn()
+  public createdAt!: Date;
+
+  @Field()
+  @UpdateDateColumn()
+  public updatedAt!: Date;
+
+  @Field((_type) => [Product])
+  @OneToMany((_type) => Product, (product: Product) => product.category)
+  public products?: Product[];
+}
